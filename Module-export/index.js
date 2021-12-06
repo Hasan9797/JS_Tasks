1d13320daf513ce40a20197ac9cdce7dbcade7e5
HaSaN_JoN <91729142+Hasan9797@users.noreply.github.com>
2021-12-06 18:36:06 +0500

    require

---
+++ Module-export/index.js
@@ -0,0 +1,4 @@
+
+const user = require('./user');
+const users = new user('Iqbol','Jonibek',34,'Iqbol@gmail.com','chilonzor','+77 345 556 1230','434wdd556xxx97');
+console.log(users.getDataToModrator());
